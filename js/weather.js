0d5d02c238ca5006514fb106afed36846fef67a5
Kiriyama <57665042+AvishekPD@users.noreply.github.com>
2020-12-03 02:18:58 +0530

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
+const key = 'a3f17ab9b65581d4188174b8374f7069'; // Noice
 
 // Set Position function
 setPosition();
